96c3c494fc180ff427173ccaf321c6f12150b994
Milan Kopp <milan.kopp@monetplus.cz>
2024-08-30 12:46:12 +0200

    hamburger mobile menu

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,7 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {}, // Doporučeno pro lepší kompatibilitu s prohlížeči
   },
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';  // Import Link pro klikatelné logo
+import Link from 'next/link';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="header-gradient">
       <div className="container mx-auto header-container">
@@ -11,20 +19,25 @@ const Header = () => {
             <Image 
               src="/logo.png" 
               alt="Veterina Svahy" 
+              width={120} 
+              height={120} 
               className="logo-image"
-              width={120}  // Případně upravte šířku podle potřeby
-              height={120} // Případně upravte výšku podle potřeby
             />
           </Link>
           <h1 className="text-2xl font-bold text-white">Veterina Svahy</h1>
         </div>
-        <nav className="nav-links">
-          <a href="/" className="nav-link">Domů</a>
-          <a href="/services" className="nav-link">Služby</a>
-          <a href="/equipment" className="nav-link">Vybavení</a>
-          <a href="/team" className="nav-link">Tým</a>
-          <a href="/contact" className="nav-link">Kontakt</a>
-          <a href="/calendar" className="nav-link">Kalendář</a>
+        <div className="hamburger-btn" onClick={toggleMenu}>
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}></path>
+          </svg>
+        </div>
+        <nav className={`nav-links ${isOpen ? 'menu-open' : ''}`}>
+          <Link href="/" className="nav-link">Domů</Link>
+          <Link href="/services" className="nav-link">Služby</Link>
+          <Link href="/equipment" className="nav-link">Vybavení</Link>
+          <Link href="/team" className="nav-link">Tým</Link>
+          <Link href="/contact" className="nav-link">Kontakt</Link>
+          <Link href="/calendar" className="nav-link">Kalendář</Link>
         </nav>
       </div>
     </header>
