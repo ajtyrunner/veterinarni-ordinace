d96acb1e4e5e9e93023e15ac76f990ee0699ef1e
Milan Kopp <milan.kopp@monetplus.cz>
2024-09-01 00:52:09 +0200

    Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Veterina Svahy',
@@ -13,6 +14,21 @@ export default function RootLayout({
   return (
     <html lang="cs">
       <body className="flex flex-col min-h-screen">
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-EHQ8T6HDD3`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EHQ8T6HDD3', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
         {children}
       </body>
     </html>
