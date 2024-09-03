bd26e385299980ed646d2ec72376feb435a87050
Milan Kopp <milan.kopp@monetplus.cz>
2024-09-03 20:38:44 +0200

    add equipment images

--- src/components/Equipment.tsx
+++ src/components/Equipment.tsx
@@ -3,18 +3,49 @@ import Image from 'next/image';
 
 const Equipment = () => {
   const equipmentItems = [
+
     {
-      title: 'RTG vyšetření',
-      description: 'Moderní rentgenové vybavení pro přesnou diagnostiku.',
-      image: '/images/rtg.jpg', // Relativní cesta k obrázku
-      alt: 'RTG vyšetření'
-    },
+        title: 'Hematologický analyzátor',
+        description: 'Analyzátor krve, který zvládne vyšetřit víc parametrů u různých druhů zvířat.',
+        image: '/images/hematalogie.jpg', // Relativní cesta k obrázku
+        alt: 'Hematologický analyzátor'
+      },    
+      {
+          title: 'Sterilizátor',
+          description: 'Autokláv MELAG SteriHero Vet je nejmodernějším nástrojem, který zajistí spolehlivou sterilizaci.',
+          image: '/images/sterilizator.jpg', // Relativní cesta k obrázku
+          alt: 'Sterilizátor'
+        },          
     {
       title: 'Sonografické vyšetření',
       description: 'Pokročilý ultrazvukový přístroj pro detailní vyšetření.',
       image: '/images/sono.jpg', // Relativní cesta k obrázku
       alt: 'Sonografické vyšetření'
-    }
+    },
+    {
+        title: 'RTG vyšetření',
+        description: 'Moderní rentgenové vybavení pro přesnou diagnostiku.',
+        image: '/images/rtg.jpg', // Relativní cesta k obrázku
+        alt: 'RTG vyšetření'
+      },  
+      {
+        title: 'Inhalační anesteziologické jednotka',
+        description: '2 inhalační anesteziologické jednotky pro bezpečné vedení anestezie, jedna na operačním sále a druhá na zubním sále.',
+        image: '/images/iaj.jpg', // Relativní cesta k obrázku
+        alt: 'Inhalační anesteziologické jednotka'
+      },
+      {
+          title: 'Pooperační péče',
+          description: 'Vyhřívané hospitalizační boxy pro drobné savce a plazy.',
+          image: '/images/klec.jpg', // Relativní cesta k obrázku
+          alt: 'Pooperační péče'
+        },
+        {
+            title: 'Štěrbinová lampa',
+            description: 'Štěrbinová lampa Kowa k oftamologickému vyšetření.',
+            image: '/images/lampa.jpg', // Relativní cesta k obrázku
+            alt: 'Štěrbinová lampa'
+          } 
   ];
 
   return (
