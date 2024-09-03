4b2cb967bad65d048283ed84d84b60f5c6d5c46f
Milan Kopp <milan.kopp@monetplus.cz>
2024-09-03 21:06:21 +0200

    add cookies banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import Script from 'next/script';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import CookieConsent from '@/components/CookieConsent';
 
 export const metadata = {
   title: 'Veterina Svahy',
@@ -12,26 +13,32 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const consent = typeof window !== 'undefined' ? localStorage.getItem('cookie-consent') : null;
+
   return (
     <html lang="cs">
       <body className="flex flex-col min-h-screen">
-        {/* Google Analytics */}
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-EHQ8T6HDD3`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-EHQ8T6HDD3', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
+        {consent && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=G-EHQ8T6HDD3`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-EHQ8T6HDD3', {
+                  page_path: window.location.pathname,
+                });
+              `}
+            </Script>
+          </>
+        )}
         {children}
         <SpeedInsights />
+        <CookieConsent />
       </body>
     </html>
   );
